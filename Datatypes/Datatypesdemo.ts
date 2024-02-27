aed2053eb3ca1463a5461cbdc0b7f08c30524447
kokanevina <kokane.vina@gmail.com>
2024-02-27 17:09:33 +0530

    commit

---
+++ Datatypes/Datatypesdemo.ts
@@ -0,0 +1,13 @@
+// typescript is superset of javascript : right
+// javascript is superset of typescript : wrong
+
+// number, boolean : primitives
+// string : literal
+// Number, String, Boolean : wrappers
+
+// custome data types
+
+let empId:number;  // : type annotation
+empId=7890;
+empId="Hi";
+empId=true;
