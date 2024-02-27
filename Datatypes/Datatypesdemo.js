155469bf39946cb10fd8978d060c894fcd3cd2d2
kokanevina <kokane.vina@gmail.com>
2024-02-27 17:51:31 +0530

    commit

--- Datatypes/Datatypesdemo.js
+++ Datatypes/Datatypesdemo.js
@@ -1,24 +1,19 @@
 // typescript is superset of javascript : right
 // javascript is superset of typescript : wrong
-
 // number, boolean : primitives
 // string : literal
 // Number, String, Boolean : wrappers
-
 // custome data types
-
-let empId:number;  // : type annotation
-empId=7890;
-
+var empId; // : type annotation
+empId = 7890;
 console.log(empId);
-document.write("<b>"+empId+"</b>");
-let bNode=document.createElement("b");
-bNode.textContent=empId+""
+document.write("<b>" + empId + "</b>");
+var bNode = document.createElement("b");
+bNode.textContent = empId + "";
 document.body.append(bNode);
-let iNode=document.createElement('i');
-iNode.append(document.createTextNode(empId+""))
+var iNode = document.createElement('i');
+iNode.append(document.createTextNode(empId + ""));
 document.body.append(iNode);
-
 //empId="Hi";
 //empId=true;
 // to compile typescript file on machine, we need typescript compiler
