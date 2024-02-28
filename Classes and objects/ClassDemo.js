2c7f4f089e0c01640419ebece027c74d59f58bee
kokanevina <kokane.vina@gmail.com>
2024-02-28 12:51:21 +0530

    commit

--- Classes and objects/ClassDemo.js
+++ Classes and objects/ClassDemo.js
@@ -5,6 +5,8 @@ var BankAccount = /** @class */ (function () {
     function BankAccount() {
     }
     BankAccount.prototype.deposit = function (deptAmt) {
+        console.log("in method.................");
+        console.log(this);
         this.accountBalance = this.accountBalance + deptAmt;
         return this.accountBalance;
     };
@@ -26,4 +28,21 @@ console.log(account1); // empty
 console.log(account1.accountNumber);
 console.log(account1.accountType);
 console.log(account1.accountBalance);
-console.log(account1.accountNumber + 1000);
+account1.accountNumber = 898989898;
+account1.accountBalance = 1000;
+account1.accountType = "salary";
+account1.customerId = 9090;
+account1.customerName = "hari";
+console.log(account1);
+var account2 = new BankAccount(); // object : instance of class
+account2.accountBalance = 6000; // instance variable
+account2.accountType = "salary";
+account2.accountType = "savings";
+account2.accountBalance = 1000;
+account2.customerId = 9999;
+account2.customerName = "poonam";
+account2.accountNumber = 12121212;
+console.log("after deposit in account1:  " + account1.deposit(56000)); // instance methods
+console.log("after deposit in account2:  " + account2.deposit(45000));
+console.log(account1 == account2);
+console.log(account1 === account2);
--- Classes and objects/ClassDemo.ts
+++ Classes and objects/ClassDemo.ts
@@ -10,26 +10,23 @@ class BankAccount{
     customerId:number;
     customerName:string;
     accountBalance: number;
-    accountType:string
-
+    accountType:string  // public
+   
     deposit(deptAmt:number):number{
-        this.accountBalance=this.accountBalance+deptAmt;
+        console.log("in method.................");
+        console.log(this);
+        this.accountBalance=this.accountBalance+deptAmt; 
         return this.accountBalance;
     }
-
     withdraw(withAmt:number):number{
-        this.accountBalance=this.accountBalance-withAmt;
-      
+        this.accountBalance=this.accountBalance-withAmt; 
         return this.accountBalance;
     }
-
     checkBalance():number{
         return this.accountBalance;
     }
 }
-
 // create (new ) , initialize:state initial value (constructor), behavior:state change, return state (calling methods)
-
 let account1:BankAccount;
 account1=new BankAccount(); // calling default contructor provided by tsc
 // 1. created, 2. initialized: undefined values 
@@ -39,3 +36,24 @@ console.log(account1.accountNumber);
 console.log(account1.accountType);
 console.log(account1.accountBalance);
 
+account1.accountNumber=898989898;
+account1.accountBalance=1000;
+account1.accountType="salary";
+account1.customerId=9090;
+account1.customerName="hari"
+console.log(account1);
+
+let account2=new BankAccount();  // object : instance of class
+account2.accountBalance=6000;  // instance variable
+account2.accountType="salary";
+account2.accountType="savings";
+account2.accountBalance=1000;
+account2.customerId=9999;
+account2.customerName="poonam"
+account2.accountNumber=12121212;
+console.log("after deposit in account1:  "+account1.deposit(56000));  // instance methods
+
+console.log("after deposit in account2:  "+account2.deposit(45000));
+
+console.log(account1==account2);
+console.log(account1===account2);
