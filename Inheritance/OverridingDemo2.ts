68b6fb3ca439848c34ac80decd62d2939b80642d
kokanevina <kokane.vina@gmail.com>
2024-02-29 16:01:06 +0530

    commit

--- Inheritance/OverridingDemo2.ts
+++ Inheritance/OverridingDemo2.ts
@@ -1,16 +1,30 @@
 class Animal{
-    eat():void{
+     eat():void{
         console.log("Animals Eat");
+    }
+
+    getEmployee():Employee{
+        return new Employee();
     }
 }
 class Tiger extends Animal{
-    eat():void{
+     eat():number{
         console.log("Tiger Eat other animals");
+        return 0;
+    }
+
+    private sleep(){
+
+    }
+
+    getEmployee(): Trainer {
+        return new Trainer();
     }
 }
 class Goat extends Animal{
     eat():void{
         console.log("Goat Eat grass and other veg"); 
+       
     }
 }
 
@@ -21,3 +35,9 @@ let goat=new Goat();
 animal.eat();
 tiger.eat(); // run time polymorphism
 goat.eat(); //run time polymorphism
+
+// rules of overriding : name and signature must be same
+// subclass o method scope must be same or increased  but not reduced
+// if return type is primitive : then must be same in subclass o method
+// if return type is void : then allowed to change in subclass o method
+// if super class method returing object then in subclass o method, covariant is allowed
