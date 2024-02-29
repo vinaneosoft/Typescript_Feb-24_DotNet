ee9582a7fdd995613c5eb5f118beea43b9456e22
kokanevina <kokane.vina@gmail.com>
2024-02-29 11:18:53 +0530

    commit

--- Classes and objects/ConstructorDemo.ts
+++ Classes and objects/ConstructorDemo.ts
@@ -3,8 +3,7 @@ class BankAccount{
     customerId:number;
     customerName:string;
     accountBalance: number;
-    accountType:string  // public
-    
+    accountType:string
     constructor(){
         // body of constr
         console.log("in constructor....");   
@@ -14,7 +13,6 @@ class BankAccount{
         this.customerId=0;
         this.customerName="ABC";
     }
-
     deposit(deptAmt:number):number{
         this.accountBalance=this.accountBalance+deptAmt; 
         return this.accountBalance;
@@ -27,22 +25,13 @@ class BankAccount{
         return this.accountBalance;
     }
 }
-
 let account1=new BankAccount(); // tsc calling default contr no-paramter
 let account2=new BankAccount(); 
 let account3=new BankAccount();
-console.log(account1);
-console.log(account2);
-console.log(account3);
-
+console.log(account1);console.log(account2);console.log(account3);
 account1.accountNumber=789066;
 account2.accountNumber=676767667;
 account3.accountNumber=45454545;
- 
-console.log(account1);
-console.log(account2);
-console.log(account3);
-
+console.log(account1);console.log(account2);console.log(account3);
 console.log(account3.accountType);
-
 console.log(account3.accountType="current");
