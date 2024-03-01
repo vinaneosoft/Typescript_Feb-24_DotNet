07c672884ba9aebd2fc16b71f4a61111f1fc3efc
kokanevina <kokane.vina@gmail.com>
2024-03-01 15:31:51 +0530

    commit

--- Generics/Wrappers/WrapperDemo.js
+++ Generics/Wrappers/WrapperDemo.js
@@ -2,12 +2,18 @@
 var s = "India is my country"; // string literal (primitive)
 var sobject = new String(s); // String  object
 if (sobject instanceof String)
-    console.log("Wrapping done");
+    console.log("Wrapping done for sobject");
 var ss;
 ss = s;
 if (ss instanceof String)
-    console.log("Wrapping done");
+    console.log("Wrapping done ss"); // will not get displayed
 var rolnum = 345; // number : primitive
 var rollObject = new Number(rolnum); // Number object
+if (rollObject instanceof Number)
+    console.log("Wrapping done for rollObject");
+var roll;
+roll = rolnum;
+if (roll instanceof Number)
+    console.log("Wrapping done for roll"); // will not get displayed
 var marriedStatus = true; // boolean
 var statusObject = new Boolean(marriedStatus); // Boolean type object
--- Generics/Wrappers/WrapperDemo.ts
+++ Generics/Wrappers/WrapperDemo.ts
@@ -1,18 +1,21 @@
 // to convert primitive to object we neer wrapper
 
 let s="India is my country"  // string literal (primitive)
-
 let sobject=new String(s); // String  object
 if(sobject instanceof String)
-    console.log("Wrapping done");
+    console.log("Wrapping done for sobject");
 let ss:String;
 ss=s;
 if(ss instanceof String)
-    console.log("Wrapping done");
-let rolnum=345; // number : primitive
+    console.log("Wrapping done ss"); // will not get displayed
 
+let rolnum=345; // number : primitive
 let rollObject=new Number(rolnum); // Number object
+if(rollObject instanceof Number)
+    console.log("Wrapping done for rollObject");
 let roll:Number;
 roll=rolnum;
+if(roll instanceof Number)
+    console.log("Wrapping done for roll") // will not get displayed
 let marriedStatus=true;  // boolean
 let statusObject=new Boolean(marriedStatus); // Boolean type object
