e33b8330bd52e393c9a970193872c78286fd4380
kokanevina <kokane.vina@gmail.com>
2024-03-01 14:30:56 +0530

    commit

--- Interfaces/NHPStudent.ts
+++ Interfaces/NHPStudent.ts
@@ -3,12 +3,10 @@ import { StudentIntf } from "./StudentIntf";
 export class NHPStudent implements StudentIntf{
     studentName: string;
     studentId:string;
-
+    rollNumber:number;
     showStudentDetails(): void {
         console.log("NHP Student id is :"+this.studentId);
         console.log(`NHP Student name is : ${this.studentName}`);
-        
-        
      
     }
 }
--- Interfaces/StudentIntf.ts
+++ Interfaces/StudentIntf.ts
@@ -1,5 +1,7 @@
 export interface StudentIntf{
     studentId:string;
     studentName:string;
+    rollNumber?:number;
     showStudentDetails():void; // method signature
+    addMarks?():number;
 }
