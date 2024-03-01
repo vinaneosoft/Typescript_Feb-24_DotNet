d22f2b375dc2b7ef0d57def82af18ebfbbf22e9c
kokanevina <kokane.vina@gmail.com>
2024-03-01 12:36:28 +0530

    commit

--- Modules/ModuleImport.ts
+++ Modules/ModuleImport.ts
@@ -1,12 +1,17 @@
 // we will import and use the Modules
 
-import { companyName,Student } from "./MyModules/module1";
+import { changeCompany, companyName,Student } from "./MyModules/module1";
 import * as neo from "./MyModules/module3";
 import {companyName as cname, Student as Stud, showCompany} from './MyModules/module2';
 /** for importing default exported properties from module do not use {} */
 import StudMarks from './MyModules/module2';
 import changeDepartment from './MyModules/module3';
+
 console.log(companyName); //module1
+/*Cannot assign to 'companyName' because it is an import.*/
+//companyName="Neosoft";
+changeCompany("Neosoft");
+console.log(companyName); 
 console.log(cname);  // module2
 
 let student=new Student(); // from module1
--- Modules/MyModules/module1.ts
+++ Modules/MyModules/module1.ts
@@ -1,7 +1,12 @@
 // module name is demo1 
 
 
-export let companyName="neosoft";
+export let companyName="neosoft tech";
+
+
+export function changeCompany(company:string){
+    companyName=company;
+}
 
 export class Student{
     protected studId:number
