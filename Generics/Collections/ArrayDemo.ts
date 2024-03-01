53e8e72e57440f4d5b57d348d37dc8e8d99c6e7f
kokanevina <kokane.vina@gmail.com>
2024-03-01 16:07:08 +0530

    commit

---
+++ Generics/Collections/ArrayDemo.ts
@@ -0,0 +1,14 @@
+
+let ageArray1=new Array<number>();
+ageArray1.push(23,56,12,34);
+
+let cityArray=new Array<string>();
+cityArray.push("mumbai","pune");
+
+
+let salaryArray=new Array<Number>();
+
+let salary1=new Number(34000);
+let salary2=new Number(34000);
+salaryArray.push(salary1,salary2);
+
--- Generics/Wrappers/WrapperDemo.ts
+++ Generics/Wrappers/WrapperDemo.ts
@@ -5,7 +5,6 @@ indiaObject=new String(india);
 
 console.log(indiaObject instanceof String);
 
-
 let salary=56000; 
 let salaryObject=new Number(salary);
 console.log(salaryObject instanceof Number);
